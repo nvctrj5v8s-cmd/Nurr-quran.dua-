f84c0f8f2dca0701c2510d836a07e45d3e9259ae
Mohammad <mohammad.shikho999@icloud.com>
2026-01-20 17:04:56 +0100

    Deploy: Mushaf-Reader Update (Seite 3, Surah-Sprung, Info entfernt, größere Bilder)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "9956621"
+    serviceWorkerVersion: "3714331846"
   }
 });
