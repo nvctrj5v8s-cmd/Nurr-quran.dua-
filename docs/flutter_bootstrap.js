259437d2b7d29e5af6118102acb302a8eb870aab
Mohammad <mohammad.shikho999@icloud.com>
2026-02-13 11:46:45 +0100

    Deploy: Complete rebuild with correct base-href

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3566283717"
+    serviceWorkerVersion: "1584461070"
   }
 });
