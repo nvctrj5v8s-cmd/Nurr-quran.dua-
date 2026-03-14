448031f100fbf0a449bf1f56bb27a82fc378f64f
Mohammad <mohammad.shikho999@icloud.com>
2026-03-14 19:31:30 +0100

    Deploy web: Dhikr intro + Home Masbaha + localized name tip

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "887311728"
+    serviceWorkerVersion: "879840112"
   }
 });
