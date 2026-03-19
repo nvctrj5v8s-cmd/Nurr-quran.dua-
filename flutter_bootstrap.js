5830b7a0c19cd594869a301a874c2264c38f82f5
Mohammad <mohammad.shikho999@icloud.com>
2026-03-19 16:13:49 +0100

    fix: include english mushaf assets in deployed web manifests

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "549917911"
+    serviceWorkerVersion: "4012167489"
   }
 });
