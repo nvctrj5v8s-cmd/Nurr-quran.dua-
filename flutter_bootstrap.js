46e549f8e98d9dddfc733ccc6ef7c007768b82cb
moham <moham@example.com>
2026-08-21 22:15:15 +0200

    Deploy expanded Sunnah web experience

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2421503237" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3712335536" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
